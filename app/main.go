fd6e8fbdfcd7e147085890b64f0cd1561c2d9df1
Victor Jacobs <victor@vjcbs.be>
2024-04-27 16:48:10 +0200

    Configurable port

--- app/main.go
+++ app/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/codecrafters-io/redis-starter-go/app/server"
 )
 
 func main() {
-	server, err := server.NewServer("0.0.0.0:6379")
+	var port string
+	flag.StringVar(&port, "port", "6379", "port on which to run the server")
+
+	flag.Parse()
+
+	server, err := server.NewServer(fmt.Sprintf("0.0.0.0:%v", port))
 	if err != nil {
 		log.Fatalf("Failed to create server: %v", err)
 	}
